c48fb1f596049fb9ed16dc015897b4e305efd3e2
Grzegorz Orlowski <g.p.orlowski@gmail.com>
2020-02-08 14:36:05 +0100

    Next changes

--- src/main/java/pl/sda/gporlowski/App.java
+++ src/main/java/pl/sda/gporlowski/App.java
@@ -1,12 +1,19 @@
 package pl.sda.gporlowski;
 
+import pl.sda.gporlowski.characters.Monster;
 import pl.sda.gporlowski.characters.Player;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Player player = PlayerCreator.createPLayer();
-        System.out.println(player);
+        List<Monster> monsters = MonsterCreationUtil.createMonsters();
+
+        monsters.get(0).receiveDamage(player.attack());
+        monsters.stream().filter(m -> m.isAlive())
+                .forEach(singleMonster -> player.receiveDamage(singleMonster.attack()));
+        System.out.println(monsters);
     }
 }
--- src/main/java/pl/sda/gporlowski/characters/Creature.java
+++ src/main/java/pl/sda/gporlowski/characters/Creature.java
@@ -7,4 +7,43 @@ public abstract class Creature {
     protected boolean alive;
     protected int strength;
     protected WeaponType weaponType;
+
+    public void receiveDamage(int dmg) {
+        hp -= dmg;
+        if (hp <= 0) {
+            alive = false;
+        }
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public WeaponType getWeaponType() {
+        return weaponType;
+    }
+
+    public void setWeaponType(WeaponType weaponType) {
+        this.weaponType = weaponType;
+    }
 }
